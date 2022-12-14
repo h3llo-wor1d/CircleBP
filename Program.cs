1adc8f23161407c5fca1dc4ec714a01d94db68d5
willow-rubenstein <contact.notashe@gmail.com>
2022-12-13 20:56:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,217 @@
+﻿using OsuMemoryDataProvider;
+using OsuMemoryDataProvider.OsuMemoryModels;
+using OsuMemoryDataProvider.OsuMemoryModels.Direct;
+using System.Diagnostics;
+using Buttplug;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System;
+using System.Timers;
+
+namespace CircleBP
+{
+    public class EnvelopeGenerator
+    {
+        
+    }
+
+    internal class Program
+
+    {
+        private static StructuredOsuMemoryReader _sreader = StructuredOsuMemoryReader.Instance.GetInstanceForWindowTitleHint("");
+        private static ButtplugClient client = new ButtplugClient("CircleBP v1.0 (PRIVATE BETA!!!) by Willow");
+        private static ButtplugClientDevice device = null;
+        private static string ip = $"ws://{ReadIP()}";
+
+        private static System.Timers.Timer aTimer;
+
+        public double envValue; // The actual value of the envelope ATM.
+        public int tickRate = 60;
+        public int currentTime = 0;
+        public double decay = .5;
+        public int runTime = 0;
+
+        // Envelope Generator Tests
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            if (currentTime < runTime)
+            {
+                currentTime++;
+                envValue = (double)currentTime / (double)runTime;
+                device.SendVibrateCmd(envValue);
+            }
+            else
+            {
+                aTimer.Enabled = false;
+            }
+        }
+
+        public void Run()
+        {
+            envValue = 0;
+            currentTime = 0;
+            runTime = (int)(decay * 60);
+            aTimer = new System.Timers.Timer(10);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        public static string ReadIP()
+        {
+            string path = Directory.GetCurrentDirectory() + "/ip.config";
+            if (File.Exists(path))
+            {
+                return File.ReadAllText(@"ip.config");
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                    sw.WriteLine("partners.ip.here");
+                return "127.0.0.1";
+            }
+        }
+        public static async Task ExecuteVibrate(int i)
+        {
+            var strengthDict = new Dictionary<int, double>()
+            {
+                {1, .75 },
+                {2, .5 },
+                {3, .25 },
+                {4, 0 }
+            };
+            device.SendVibrateCmd(strengthDict[i]);
+        }
+
+        public static async Task Main()
+        {
+
+            var currentScore = new string[4];
+            Console.WriteLine($"Using IP {ip}");
+            var connector = new ButtplugWebsocketConnectorOptions(
+                new Uri(ip));
+            try
+            {
+                await client.ConnectAsync(connector);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            await client.StartScanningAsync();
+            await client.StopScanningAsync();
+            device = client.Devices[0];
+
+            Console.WriteLine("CircleBP v1.0 (PRIVATE BETA) || By Willow \n\nPlease Keep This Window Open!!!");
+
+            await Task.Run(async () =>
+            {
+                Stopwatch stopwatch;
+                _sreader.WithTimes = true;
+                var baseAddresses = new OsuBaseAddresses();
+                bool devicePlay = false;
+                string curComboColor = "";
+
+
+                while (true)
+                {
+
+                    stopwatch = Stopwatch.StartNew();
+                    _sreader.TryRead(baseAddresses.GeneralData); // Initialize data reader
+
+                    if (baseAddresses.GeneralData.OsuStatus == OsuMemoryStatus.SongSelect)
+                    {
+                        currentScore = new string[4];
+                        curComboColor = "";
+                        device.SendVibrateCmd(0.0);
+                        if (devicePlay)
+                        {
+                            devicePlay = false;
+
+                        }
+                    }
+                    else
+                        baseAddresses.SongSelectionScores.Scores.Clear();
+
+                    if (baseAddresses.GeneralData.OsuStatus == OsuMemoryStatus.ResultsScreen)
+                    {
+                        if (devicePlay)
+                        {
+                            device.SendVibrateCmd(0.0);
+                            currentScore = new string[4];
+                            curComboColor = "";
+                            devicePlay = false;
+                        }
+                    }
+
+
+                    if (baseAddresses.GeneralData.OsuStatus == OsuMemoryStatus.Playing)
+                    {
+                        if (!devicePlay)
+                        {
+                            devicePlay = true;
+                            device.SendVibrateCmd(0.0);
+                        }
+
+                        _sreader.TryReadProperty(baseAddresses.Player, nameof(Player.Hit300), out var hit300);
+                        _sreader.TryReadProperty(baseAddresses.Player, nameof(Player.Hit100), out var hit100);
+                        _sreader.TryReadProperty(baseAddresses.Player, nameof(Player.Hit50), out var hit50);
+                        _sreader.TryReadProperty(baseAddresses.Player, nameof(Player.HitMiss), out var hitmiss);
+
+                        try
+                        {
+                            List<String> testList = new List<String>();
+                            testList.Add(hit300.ToString());
+                            testList.Add(hit100.ToString());
+                            testList.Add(hit50.ToString());
+                            testList.Add(hitmiss.ToString());
+
+                            for (int i = 0; i < testList.Count; i++)
+                            {
+                                if (currentScore[i] != testList[i])
+                                {
+                                    currentScore[i] = testList[i];
+                                    if (testList[i] != "0")
+                                    {
+                                        if (i.ToString() != curComboColor)
+                                        {
+                                            await ExecuteVibrate(i + 1);
+                                            curComboColor = i.ToString();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            if (devicePlay)
+                            {
+                                currentScore = new string[4];
+                                devicePlay = false;
+                                curComboColor = "";
+                                device.SendVibrateCmd(0.0);
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (devicePlay)
+                        {
+                            currentScore = new string[4];
+                            devicePlay = false;
+                            device.SendVibrateCmd(0.0);
+                        }
+                    }
+
+                    stopwatch.Stop();
+
+                    _sreader.ReadTimes.Clear();
+                    await Task.Delay(33);
+                }
+            });
+        }
+    }
+}
---
+++ simpad.cs
@@ -0,0 +1,90 @@
+﻿using Device.Net;
+using Microsoft.Extensions.Logging;
+using Hid.Net.Windows;
+using System.Drawing;
+
+namespace CircleRGB_v2
+{
+    internal class Simpad
+    {
+        public static IDevice? instance;
+        private static async Task sendBuffer(byte[] buffer)
+        {
+            await instance.WriteAsync(buffer).ConfigureAwait(false);
+        }
+
+        public async Task setMode(string mode)
+        {
+            var buffer = new byte[65];
+            buffer[1] = 0x08;
+            buffer[3] = buffer[4] = 0xFF;
+            buffer[5] = 0x04;
+            switch (mode) {
+                case "rainbow":
+                    buffer[2] = 0x06;
+                    buffer[6] = 0x02;
+                    break;
+                case "on":
+                    buffer[2] = 0x02;
+                    buffer[6] = 0x06;
+                    break;
+                case "reset":
+                    buffer[2] = buffer[3] = buffer[4] = 0x00;
+                    buffer[5] = buffer[6] = 0x04;
+                    break;
+            }
+            await sendBuffer(buffer);
+        }
+
+        public async Task setKeypad(string color)
+        {
+            Color rgb = ColorTranslator.FromHtml(color);
+            var buffer = new byte[65];
+            buffer[1] = 0x06; // Key
+            buffer[2] = rgb.R; // R
+            buffer[3] = rgb.G; // G
+            buffer[4] = rgb.B; // B
+            buffer[5] = 0x04; // Brightness
+            buffer[6] = (byte)(buffer[2] ^ buffer[3] ^ buffer[4] ^ buffer[5]); // Still no idea what this is
+            await sendBuffer(buffer);
+            buffer[1] = 0x07;
+            await sendBuffer(buffer);
+        }
+
+        public async void Init()
+        {
+
+            var loggerFactory = LoggerFactory.Create((builder) =>
+            {
+                _ = builder.AddDebug().SetMinimumLevel(LogLevel.Trace);
+            });
+
+            //Register the factory for creating Hid devices. 
+            var hidFactory =
+                new FilterDeviceDefinition(vendorId: 0x8088)
+                .CreateWindowsHidDeviceFactory(loggerFactory);
+
+            //Register the factory for creating Usb devices.
+            var usbFactory =
+                new FilterDeviceDefinition(vendorId: 0x8088)
+                .CreateWindowsHidDeviceFactory(loggerFactory);
+
+
+            var factories = hidFactory.Aggregate(usbFactory);
+
+            //Get connected device definitions
+            var deviceDefinitions = (await factories.GetConnectedDeviceDefinitionsAsync().ConfigureAwait(false)).ToList();
+
+            if (deviceDefinitions.Count() == 0)
+            {
+                return;
+            }
+
+            //Get the device from its definition
+            instance = await hidFactory.GetDeviceAsync(deviceDefinitions[1]).ConfigureAwait(false);
+
+            //Initialize the device
+            await instance.InitializeAsync().ConfigureAwait(false);
+        }
+    }
+}
